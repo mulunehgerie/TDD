e673ff6e07101dd64c8403834fc2c607f5740d1c
Haftom Abrha <haftomabrha452@gmail.com>
2024-05-17 04:32:56 +0300

    Test remove method implemented

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -14,6 +14,18 @@ public class LinkedListTest {
         list.add(5);
         list.printList();
     }
+    @Test
+    public void testRemove() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.remove(3);
+        System.out.println("List after remove");
+        list.printList();
+    }
 
     
 }

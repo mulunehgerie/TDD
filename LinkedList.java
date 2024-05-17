11c99c2301d8269a84279aa24e4a95a0b473c96d
Haftamu Redae <haf0149@gmail.com>
2024-05-17 06:04:57 +0300

    I have LinkedList java class with add() method

---
+++ LinkedList.java
@@ -0,0 +1,18 @@
+public class LinkedList {
+    ListNode head;
+    int size;
+    public void add(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        
+    }
+    
+}
